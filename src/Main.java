ea70a228fdca3b8ef07cf0b4cbdda7f1490246ce
mariaasir <maria.l.paton@gmail.com>
2024-10-18 11:46:08 +0200

    Actualización Clases DTO y DAO y creación del menú

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.sql.SQLException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
+    static Conexion conexionBdd = new Conexion();
+    static LibroDAO libro = new LibroDAO(conexionBdd.conectar());
+    static Scanner scanner = new Scanner(System.in);
 
+
+    public static void menu() throws SQLException {
+
+        System.out.println("----------------------MENÚ----------------------");
+        System.out.printf("1. CREAR LIBRO \n2. CREAR AUTOR\n3. CREAR USUARIO\n 4.CREAR PRÉSTAMO\n");
+        System.out.printf("5. LEER LIBROS\n6. LEER AUTORES\n7. LEER USUARIOS\n 8.LEER PRÉSTAMOS\n");
+        System.out.printf("9. ACTUALIZAR LIBRO\n10. ACTUALIZAR AUTOR\n 11.ACTUALIZAR USUARIO \n12.ACTUALIZAR PRESTAMO\n");
+        System.out.printf("13. ELIMINAR LIBRO\n14. ELIMINAR AUTOR\n15.ELIMINAR USUARIO\nELIMINAR PRÉSTAMO\n");
+        System.out.println("11. SALIR\n");
+        System.out.print("Introduce una opción: ");
+        int opcion = scanner.nextInt();
+
+
+        switch (opcion){
+            case 1:
+                libro.crearLibro();
+                menu();
+
+            case 5:
+                libro.leerLibros();
+                menu();
+            case 9:
+                libro.actualizarLibro();
+                menu();
+            case 13:
+                libro.eliminarLibro();
+                menu();
+            default:
+                System.out.println("ERROOOOOOR");
+        }
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        menu();
+    }
 
 }
+
+
