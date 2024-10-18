6151f7fe06d07147303143c6c11b8491ecbf6dbb
mariaasir <maria.l.paton@gmail.com>
2024-10-18 10:44:15 +0200

    Actualización Clases DTO y creación de clase Conexion

--- src/AutorDTO.java
+++ src/AutorDTO.java
@@ -1,2 +1,29 @@
 public class AutorDTO {
+    int id;
+    String autor;
+
+    public AutorDTO(String autor) {
+        this.autor = autor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return  id + ", "+ autor;
+    }
 }
